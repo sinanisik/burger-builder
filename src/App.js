1a723ca7de456cfd7b815f9c1848f51fe0207968
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-04-28 16:52:07 +0300

    structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
