a81b13c9232084dacac6464e5e02a144768197db
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-06-21 19:06:56 +0300

    Display user specific orders

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -26,7 +26,7 @@ export const purchaseBurger = (orderData, token) => {
   return (dispatch) => {
     dispatch(purhcaseBurgerStart());
     axios
-      .post("/orders.json?auth=", +token, orderData)
+      .post("/orders.json?auth=" + token, orderData)
       .then((response) => {
         console.log(response.data);
         dispatch(purchaseBurgerSuccess(response.data.name, orderData));
@@ -63,11 +63,12 @@ export const fetchOrdersStart = () => {
   };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
   return (dispatch) => {
     dispatch(fetchOrdersStart());
+    const queryParams = `?auth=${token}&orderBy="userId"&equalTo="${userId}"`;
     axios
-      .get("/orders.json?auth=" + token)
+      .get(`/orders.json${queryParams}`)
       .then((res) => {
         const fetchedOrders = [];
         for (let key in res.data) {
