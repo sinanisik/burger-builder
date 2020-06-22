6afeb64dc2cb924943191cf37026e52558ee6b96
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-06-22 13:40:41 +0300

    Add lazy loading

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import burgerLogo from "../../assets/images/burger-logo.png";
 import classes from "./Logo.module.css";
-import { checkPropTypes } from "prop-types";
 
 const Logo = (props) => (
   <div className={classes.Logo} style={{ height: props.height }}>
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -58,7 +58,6 @@ export const auth = (email, password, isSignup) => {
     axios
       .post(url, authData)
       .then((response) => {
-        console.log(response);
         const expirationDate = new Date(
           new Date().getTime() + response.data.expiresIn * 1000
         );
@@ -69,7 +68,6 @@ export const auth = (email, password, isSignup) => {
         dispatch(checkAuthTimeout(response.data.expiresIn));
       })
       .catch((err) => {
-        console.log(err);
         dispatch(authFail(err.response.data.error));
       });
   };
